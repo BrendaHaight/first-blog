43b88ccf1650c45d0ae54bef859555dff6fe8d7a
Brenda Haight <brendaaureliano9@gmail.com>
2024-03-24 07:47:53 -0500

    adding comments

--- js/blog.js
+++ js/blog.js
@@ -25,6 +25,4 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const backButton = document.getElementById("back-btn");
-backButton.addEventListener("click", function () {
-  window.location.href = "../index.html";
-});
+ba
